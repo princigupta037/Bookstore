75081ab4e0b0fdd5148bca00dfd80d5f5de8e0c2
Princi Gupta <princygupta037@gmail.com>
2019-12-12 13:01:09 +0530

    frontend

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import *
 
+
 admin.site.register(Computer)
 admin.site.register(Electronics)
 admin.site.register(Electrical)
 admin.site.register(Mechanical)
+admin.site.register(civil)
